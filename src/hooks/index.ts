a223eff584195aae574dafb1e780fde1cd7c005c
Emanuel Lourenço <116807736+ecorreialourenco@users.noreply.github.com>
2025-03-06 11:46:52 +0000

    reorganize hooks folder

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,2 @@
 export * from "./mutations";
-
-export * from "./useAgent";
-export * from "./useContracts";
-export * from "./useExtraction";
-export * from "./useFactions";
-export * from "./useHeadquarters";
-export * from "./useMarket";
-export * from "./useShips";
-export * from "./useShipyard";
-export * from "./useShipyardFinder";
-export * from "./useWaypoint";
+export * from "./queries";
